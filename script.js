d1bb383f93310c44b8df70cb47bb77ee63bcacf5
alopezarias <47595368+alopezarias@users.noreply.github.com>
2021-04-13 00:16:51 +0200

    Casi terminada

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ var arrayPalabras4 = [];
 var arrayPalabras6 = [];
 var casillas = [];
 var valorCasillas = [];
+var filasErroneas = [];
 var letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'];
 var guardarDatos = false;
 var pistas = 3;
+var infoPasatiempo = {'valores': '', 'pistas': ''};
+var soluciones = {'0': 'clan', '5':'pena', '6':'remato', '11':'torero'};
+var colores = {'verde':'#A7F270', 'rojo':'#E66852'};
 
 /*FUNCIONES DE LA CARGA DEL DICCIONARIO Y DEMÁS INFORMACIÓN INICIAL */
 
 function cargarDiccionario(){
-    let url = "https://ordenalfabetix.unileon.es/aw/diccionario.txt";
+    //let url = "https://ordenalfabetix.unileon.es/aw/diccionario.txt";
+    let url = "https://diccionario.casasoladerueda.es/diccionario.txt";
     fetch(url)
         .then(response => response.text())
         .then((response) => {
@@ -29,7 +34,7 @@ function almacenarPalabras(texto){
             arrayPalabras6.push(elemento);
         }
     });
-    console.log("Arrays cargados, ya se puede resolver");
+    //console.log("Arrays cargados, ya se puede resolver");
     document.getElementById("botonResolver").disabled = false;
     document.getElementById("botonResolver").enabled = true;
 }
@@ -58,26 +63,24 @@ function guardarReferenciasCasillas(){
 
 function cargarInfoGuardada(){
     guardarReferenciasCasillas();
-
-    let valores, num_pistas;
-
     if (typeof(Storage) !== "undefined") {
-        valores = localStorage.getItem("valor_casillas");
-        num_pistas = localStorage.getItem("pistas");
-        if(valores !== null){
+        let info = localStorage.getItem("pasatiempo");
+        //console.log(JSON.parse(info));
+        if(info !== null){
+            infoPasatiempo = JSON.parse(info);
             document.getElementById("guardar").checked = true;
             guardarDatos = true;
-            console.log(JSON.parse(valores));
-            cargarValoresCasillas(JSON.parse(valores));
-            this.pistas = parseInt(num_pistas);
-            bajarPista();
+            if(infoPasatiempo.valores.length > 0)
+                cargarValoresCasillas(infoPasatiempo.valores);
+            this.pistas = parseInt(infoPasatiempo.pistas);
+            actualizarPistas();
         }else{
             document.getElementById("guardar").checked = false;
         }
     } else {
         alert("Sorry, your browser does not support Web Storage...");
     }
-    console.log("Quedan " + this.pistas + " pistas")
+    //console.log("Quedan " + this.pistas + " pistas")
 }
 
 function cargarValoresCasillas(valores){
@@ -109,15 +112,18 @@ function almacenamiento(){
         alert("SE GUARDARÁ EL PROGRESO");
         guardarEnMemoria();
     }else{
-        localStorage.removeItem("valor_casillas");
-        localStorage.removeItem("pistas");
+        localStorage.removeItem("pasatiempo");
         alert("EL PROGRESO NO SE GUARDARÁ");
     }
 }
 
 function guardarEnMemoria(){
+    
+    infoPasatiempo.valores = valorCasillas;
+    infoPasatiempo.pistas = pistas;
+    
     if (typeof(Storage) !== "undefined") {
-        localStorage.setItem("valor_casillas", JSON.stringify(valorCasillas));
+        localStorage.setItem("pasatiempo", JSON.stringify(infoPasatiempo));
     } else {
         alert("Sorry, your browser does not support Web Storage...");
     }
@@ -127,12 +133,7 @@ function guardarCasillas(){
     guardarValorCasillas();
     if(guardarDatos){
         guardarEnMemoria();
-        guardarPistas();
     }
-}
-
-function guardarPistas(){
-    localStorage.setItem("pistas", this.pistas);
 }
 
 function guardarValorCasillas(){
@@ -196,17 +197,17 @@ function otorgarPista(){
         });
         document.getElementById("pista").value = texto;
         this.pistas--;
-        bajarPista();
+        actualizarPistas();
     }else{
         alert("NO HAY MÁS PISTAS DISPONIBLES");
     }
-    guardarPistas();
+    guardarEnMemoria();
 }
 
-function bajarPista(){
+function actualizarPistas(){
     let texto = document.getElementById("botonPista").innerText;
     let nuevo = "";
-    nuevo = nuevo + texto.slice(0, 13) + pistas + ")"
+    nuevo = nuevo + texto.slice(0, 13) + pistas + ")";
     document.getElementById("botonPista").innerText = nuevo;
     if(pistas<1){
         document.getElementById("botonPista").enabled = false;
@@ -217,7 +218,7 @@ function bajarPista(){
 function eliminarCaracter(palabra, car){
     let final = "";
     let indice = palabra.indexOf(car);
-    console.log(palabra + ", " + car + " -> " + indice);
+    //console.log(palabra + ", " + car + " -> " + indice);
     final = final + palabra.slice(0,indice);
     if(indice+1 < palabra.length){
         final = final + palabra.slice(indice+1, palabra.length);
@@ -230,14 +231,127 @@ function eliminarCaracter(palabra, car){
 function resolverPasatiempo(){
     /*Comprobar si todas las palabras son correctas y si cumplen las condiciones de
     cambio de letra y permutacion */
-    alert("Alerta de Boton");
+    
+    let palabra = "";
+    let anterior = null;
+    filasErroneas = [];
+    var error = false;
+
+    valorCasillas.forEach(function(elemento, indice, array){
+        elemento.forEach(function(elemento, indice, array){
+            palabra += elemento;
+        });
+        if(indice<6){
+            if(error){
+                if(indice == 4 && indice != 5) error = false;
+                filasErroneas.push(indice);
+            }else{
+                if(!validarFila(palabra,indice,anterior)){
+                    filasErroneas.push(indice);
+                    error = true;
+                }
+            }
+        }else{
+            if(error && indice != 11){
+                if(indice == 10) error = false;
+                filasErroneas.push(indice);
+            }else{
+                if(!validarFila(palabra,indice,anterior)){
+                    filasErroneas.push(indice);
+                    error = true;
+                }
+            }
+        }
+        anterior = palabra;
+        palabra = "";
+    });
+    //console.log("Filas erroneas: " + filasErroneas)
+    corregirTablero();
 }
 
+function validarFila(palabra, indice, anterior){
+    let solucion = null;
+    palabra = palabra.toLowerCase();
+    if(soluciones.hasOwnProperty(indice)){
+        //console.log("Es de las incluidas --> " + indice);
+        solucion = soluciones[indice];
+        if(solucion == palabra){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        //console.log("Es del usuario incluidas --> " + indice);
+        if(existe(palabra)){
+            if(validarCambio(palabra, anterior)){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
 
+function existe(palabra){
+    if(palabra.length == 4){
+        return arrayPalabras4.includes(palabra);
+    }else if(palabra.length == 6){
+        return arrayPalabras6.includes(palabra);
+    }else{
+        return false;
+    }
+}
 
+function validarCambio(palabra, anterior){
+    anterior = anterior.toLowerCase();
+    //console.log("palabra > " + palabra + " - anterior > " + anterior);
+    var longitud = palabra.length;
+    let palabraMod, todasLetras;
+    let ant;
+    for(var i=0; i<longitud; i++){
+        ant = anterior;
+        palabraMod = eliminarCaracter(palabra, palabra.charAt(i));
+        //console.log("palabra-1 letra --> " + palabraMod);
+        todasLetras = true
+        //anterior.slice(0,i) + anterior.slice(i+1,longitud);
+        for(var j=0; j<longitud-1; j++){
+            //console.log(palabraMod[j]);
+            //console.log(ant);
+            if(!ant.includes(palabraMod[j])){
+                todasLetras = false;
+            }else{
+                ant = eliminarCaracter(ant, palabraMod[j]);
+            }
+           // console.log("ant --> " + ant);
+            //console.log(todasLetras);
+        }
+        if(todasLetras) return true;
+    }
+    return false;
+}
 
+function corregirTablero(){
+    //console.log(casillas);
+    casillas.forEach(function(elemento, indice, array){
+        //console.log(elemento);
+        var funcion = pintarVerde;
+        if(filasErroneas.includes(indice))
+            funcion = pintarRojo;
+        elemento.forEach(function(elemento, indice, array){
+            //console.log(elemento);
+            funcion(elemento);
+        });
+    });
+}
 
+function pintarVerde(elemento){
+    //console.log(elemento);
+    elemento.style.backgroundColor=colores.verde;
+}
 
-
-
-
+function pintarRojo(elemento){
+    //console.log(elemento);
+    elemento.style.backgroundColor=colores.rojo;
+}
